d0b6d3447c4b5c594e8937e9b4d7b5378049c717
Yozheeqq <yozheeq@devmosh.ru-central1.internal>
2025-02-14 10:09:07 +0000

    second commit

--- src/hello.cpp
+++ src/hello.cpp
@@ -28,7 +28,7 @@ std::string SayHelloTo(std::string_view name) {
     name = "unknown user";
   }
 
-  return fmt::format("You are {}!\n", name);
+  return fmt::format("You are awesome, {}!\n", name);
 }
 
 void AppendHello(userver::components::ComponentList &component_list) {
